41fa83432b78c0c84e90725738d46e84b1ebd5ff
Anthony Tleiji <anthonytleiji@gmail.com>
2023-09-23 15:31:10 +0300

    Added mock data using Faker.

--- src/main/java/com/anthonytlei/spring_data_jpa_demo/SpringDataJpaDemoApplication.java
+++ src/main/java/com/anthonytlei/spring_data_jpa_demo/SpringDataJpaDemoApplication.java
@@ -1,13 +1,11 @@
 package com.anthonytlei.spring_data_jpa_demo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.github.javafaker.Faker;
 
 @SpringBootApplication
 public class SpringDataJpaDemoApplication {
@@ -19,22 +17,21 @@ public class SpringDataJpaDemoApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
-			Student student1 = new Student("student1", "student1", "student1@user.com", 21);
-			Student student2 = new Student("student2", "student2", "student2@user.com", 22);
-			List<Student> students = new ArrayList<Student>();
-			students.add(student1);
-			students.add(student2);
-			studentRepository.saveAll(students);
-			// Optional<Student> studentOptional = studentRepository.findByEmail("student1@user.com");
-			// if (studentOptional.isPresent()) {
-			// 	System.out.println("Found student");
-			// }
-			// studentRepository.findByFirstName("student1").forEach(System.out::println);
-			// studentRepository.findByFirstNameNative("student1").forEach(System.out::println);
-			// studentRepository.findByAgeIsGreaterThan(20).forEach(System.out::println);
-			// studentRepository.deleteByEmail("student1@user.com");
-			// studentRepository.findAll().forEach(System.out::println);
+			generateRandomStudents(studentRepository);
 		};
+	}
+
+	private void generateRandomStudents(StudentRepository studentRepository) {
+		Faker faker = new Faker();
+		for (int i=0; i < 20; ++i) {
+			String firstName = faker.name().firstName();
+			String lastName = faker.name().lastName();
+			String email = String.format("%s.%s@user.com", firstName, lastName);
+			int age = faker.number().numberBetween(17,55);
+			Student student = new Student(firstName, lastName, email, age);
+			studentRepository.save(student);
+		}
+		studentRepository.findAll().forEach(System.out::println);
 	}
 
 }
